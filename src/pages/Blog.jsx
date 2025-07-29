487db1c8751c16daf6ea9f42d1c07e58f3f05275
hungngo705 <hungnkse180346@fpt.edu.vn>
2025-07-30 02:04:05 +0700

    add search feature to consultant appoinment tab, fix small bugs

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -319,7 +319,7 @@ function Blog() {
         )}
 
         {/* Newsletter section */}
-        <div className="mt-16 bg-indigo-50 rounded-2xl p-8 md:p-10">
+        {/* <div className="mt-16 bg-indigo-50 rounded-2xl p-8 md:p-10">
           <div className="max-w-2xl mx-auto text-center">
             <h3 className="text-2xl font-bold text-gray-900 mb-4">
               Đăng ký nhận bản tin
@@ -344,7 +344,7 @@ function Blog() {
               bất cứ lúc nào.
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
